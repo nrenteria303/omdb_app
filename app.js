61f14a3f21258a05b1ef74d56e4245dcc94439bf
Nick Renteria <Nick Renteria>
2019-11-07 22:07:14 -0700

    error.ejs; ratings

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+// app.get("/error", function(req, res) {
+
+// });
+
 app.post("/", function(req, res) {
     var title = req.body.title;
     var year = req.body.year;
@@ -27,14 +31,29 @@ app.post("/", function(req, res) {
             console.log("error: " + err);                
         } else if (JSON.parse(body).Response === "False") {
             console.log("No movie found");
+            res.render("error");
         } else {
             var movieData = JSON.parse(body);
+            var IMDb = movieData.Ratings[0].Value;
+            var RT = movieData.Ratings[1].Value;
+            var MC = movieData.Ratings[2].Value;
+            // var IMDbvalue = IMDb.split("/")[0];
+            // var RTvalue = RT.split("%")[0];
+            // var MCvalue = MC.split("/")[0];
+            // var IMDbnum = parseFloat(IMDbvalue);
             res.render("movie", {
                 movieTitle: movieData.Title,
                 movieYear: movieData.Year,
+                movieDirector: movieData.Director,
                 movieCast: movieData.Actors,
                 moviePlot: movieData.Plot,
-                moviePoster: movieData.Poster
+                moviePoster: movieData.Poster,
+                ratingIMDB: IMDb,
+                ratingRT: RT,
+                ratingMC: MC,
+                // IMDbvalue: IMDbnum,
+                // RTvalue: RTvalue,
+                // MCvalue: MCvalue
             })
         }
     });
